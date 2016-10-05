58aeac6134d841d4566e08eef29110be80d78e03
SK207_24 <SK207_24@K207_24.carrollcc.edu>
2016-10-05 18:44:38 -0400

    not finish

---
+++ Lab3ch11/src/Employee.java
@@ -0,0 +1,20 @@
+import java.util.Formatter;
+public class Employee {
+String name;
+String number;
+int date;
+public Employee (String name,String number, int data){
+	this.name=name;
+	this.date=data;
+	this.number=number;
+	Formatter fm= new Formatter();
+}
+public void setname(String name){
+	name=name;
+}
+public String getname(){
+	return name;
+}
+public void setnumber(String number)
+number=
+}
